eadf01ce1737e20f08bbe7eb3761e19f43735ac9
ZZKIM <rkgml424@naver.com>
2023-08-12 22:00:28 +0900

    feat : 유저의 모든 답글 get controller 추가

--- ditto/src/main/java/com/ditto/ditto/controller/DonorController.java
+++ ditto/src/main/java/com/ditto/ditto/controller/DonorController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/donor")
@@ -16,5 +18,9 @@ public class DonorController {
     public ResponseEntity<HttpStatus> createDonor(DonorDto.Request dto, @PathVariable(name = "helperId") Long helperId){
         donorService.createDonor(dto, helperId);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @GetMapping("/{helperId}")
+    public ResponseEntity<List<DonorDto.Response>> getDonorAllByHelper(@PathVariable(name = "helperId") Long helperId){
+        return new ResponseEntity<>(donorService.getAllByHelper(helperId), HttpStatus.OK);
     }
 }
